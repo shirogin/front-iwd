784bac0197f211c784ba8c9fbc79d4d669d87ed6
Madadi youcef <tahher@live.fr>
2022-03-25 10:15:38 +0100

    saving home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import logo from "../images/logo.svg"
 import "./Home.css"
 const Home = () => {
     return <div className="Home">
-         <nav><img src={logo}/></nav>
+         <nav><img src={logo}/> </nav>
     </div>;
 };
 export default Home;
