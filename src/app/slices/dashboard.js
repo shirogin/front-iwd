f2aab5a8f04df0dc448a580c2387861ba4fe47dd
Madadi youcef <tahher@live.fr>
2022-03-25 10:03:00 +0100

    Comp

--- src/app/slices/dashboard.js
+++ src/app/slices/dashboard.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initial_state = JSON.parse(
     localStorage.getItem("Dashboard") || "null"
-) || { progress: {}, history: [] };
+) || { history: [] };
 
 const dashboard = createSlice({
     name: "dashboard",
