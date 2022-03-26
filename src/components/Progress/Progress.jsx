a2ef967b4e9455acfbb6af5baf6088ded99ef07b
Madadi youcef <tahher@live.fr>
2022-03-26 15:21:52 +0100

    cmt rmv

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -5,39 +5,7 @@ import Subjects from "../Subjects/Subjects";
 import Data from "../../status/Semester.json";
 import "./Progress.css";
 const Progress = () => {
-    const /*subjects = [
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238772/lwd/math_1_vcsids.png",
-            name: "Mathematics",
-            progress: 0,
-        },
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238765/lwd/physics_1_liwtlc.png",
-            name: "Physics",
-            progress: 20,
-        },
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238764/lwd/dictionary_1_jplzzz.png",
-            name: "English",
-            progress: 40,
-        },
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238763/lwd/history_1_bnlvej.png",
-            name: "History",
-            progress: 60,
-        },
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238761/lwd/dna_1_hn5tfb.png",
-            name: "Science",
-            progress: 80,
-        },
-        {
-            image: "https://res.cloudinary.com/shirogin/image/upload/v1648238756/lwd/quran_2_1_fo6wom.png",
-            name: "Islamic",
-            progress: 100,
-        },
-    ],*/
-        dashboard = useAppSelector((state) => state.dashboard),
+    const dashboard = useAppSelector((state) => state.dashboard),
         user = useAppSelector((state) => state.user),
         dispatch = useAppDispatch(),
         modules = Data[user.studyLevel][dashboard.progress.trimester].modules;
